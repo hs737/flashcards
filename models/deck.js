af170e23dae69bc1f8f06bccbfcf59ca8a8e5709
Harsh Sinha <hs1294@nyu.edu>
2015-12-08 07:39:28 -0500

    Added mongoose models

---
+++ models/deck.js
@@ -0,0 +1,13 @@
+/** Dependencies **/
+var mongoose = require('mongoose');
+
+/** Global Variables **/
+var Schema = mongoose.Schema;
+var deckSchema = new Schema({
+    name: String,
+    fields: [String]
+});
+var Deck = mongoose.model("Deck", deckSchema, "decks");
+
+/** Main **/
+model.exports = Deck;
